11e1c1eb9505133d2eb1cf74ea5bc845a951678d
Antti Ketola <antti@anttiketola.com>
2018-11-13 10:07:09 +0200

    Poistettu turhat importit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/KassapaateTest.java
@@ -1,9 +1,6 @@
 package com.mycompany.unicafe;
 
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
