df1e33425ae06d74e8783bc0d216667455c4d1a9
Antti Ketola <antti@anttiketola.com>
2018-11-20 15:46:39 +0200

    Lisätty muutama testi

--- Columnspeli/src/test/java/columnspeli/domain/GameAreaTest.java
+++ Columnspeli/src/test/java/columnspeli/domain/GameAreaTest.java
@@ -11,4 +11,18 @@ public class GameAreaTest {
         GameArea testArea = new GameArea(10, 10);
         assertEquals(false, testArea.hasBlock(5, 5));
     }
+    
+    @Test
+    public void getAreaEdgeXReturnsCorrectValue () {
+        GameArea testArea = new GameArea(20, 30);
+        assertEquals(20, testArea.getAreaEdgeX());
+    }
+    
+    @Test
+    public void getAreaEdgeYReturnsCorrectValue () {
+        GameArea testArea = new GameArea(20, 30);
+        assertEquals(30, testArea.getAreaEdgeY());
+    }
+    
+    
 }
